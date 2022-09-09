41f7885da61a23070f8df37ff59e072d454fc246
Ruben Chester Dieutchou <ruben.chester.dieutchou@mni.thm.de>
2022-09-09 16:37:57 -0700

    Add 102-print_comb5 file

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * A C program that prints all possible combinations of two two-digit numbers.
+ *
+ * The numbers should range from 0 to 99
+ * The two numbers should be separated by a space
+ * All numbers should be printed with two digits.
+ * 1 should be printed as 01
+ * The combination of numbers must be separated by comma,
+ * followed by a space
+ * The combinations of numbers should be printed in ascending order
+ * 00 01 and 01 00 are considered as the same combination of the
+ * numbers 0 and 1
+ * You can only use the putchar function (every other function
+ * (printf, puts, etc…) is forbidden)
+ * You can only use putchar eight times maximum in your code
+ * You are not allowed to use any variable of type char
+ * All your code should be in the main function
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 99; i++)
+	{
+		for (j = 0; j <= 99; j++)
+		{
+			if (i < j)
+			{
+				putchar((i / 10) + '0');
+				putchar((i % 10) + '0');
+				putchar(' ');
+				putchar((j / 10) + '0');
+				putchar((j % 10) + '0');
+				if (!(j == 99 && i == 98))
+				{
+					putchar(',');
+					putchar(' ');
+					}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
