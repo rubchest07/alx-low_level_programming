cb8968eb54349f39f420203aaddde2554529eb87
Ruben Chester Dieutchou <ruben.chester.dieutchou@mni.thm.de>
2022-10-07 05:52:33 -0700

    Add all tasks

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,158 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _print - moves a string one place to the left and prints the string
+ * @str: string to move
+ * @l: size of string
+ * Return: void
+ */
+void _print(char *str, int l)
+{
+	int i, j;
+
+	i = j = 0;
+	while (i < l)
+	{
+		if (str[i] != '0')
+			j = 1;
+		if (j || i == l - 1)
+			_putchar(str[i]);
+		i++;
+	}
+
+	_putchar('\n');
+	free(str);
+}
+
+/**
+ * init - initializes a string
+ * @str: sting to initialize
+ * @l: length of strinf
+ * Return: void
+ */
+void init(char *str, int l)
+{
+	int i;
+
+	for (i = 0; i < l; i++)
+		str[i] = '0';
+	str[i] = '\0';
+}
+
+char *multiply(char n, char *num, int num_index, char *dest, int dest_index);
+
+/**
+ * onlyNumbers - determines if string has only numbers
+ * @c: input string
+ * Return: 0 if false, 1 if true
+ */
+int onlyNumbers(char **c)
+{
+	int i, j;
+
+	for (i = 1; i < 3; i++)
+	{
+		for (j = 0; c[i][j]; j++)
+		{
+			if (c[i][j] < '0' || c[i][j] > '9')
+				return (1);
+		}
+	}
+	return (0);
+}
+
+/**
+ * main - A C program that multiplies two positive numbers.
+ *
+ * Usage: mul num1 num2
+ * num1 and num2 will be passed in base 10
+ * Print the result, followed by a new line
+ * If the number of arguments is incorrect, print Error,
+ * followed by a new line, and exit with a status of 98
+ * num1 and num2 should only be composed of digits.
+ * If not, print Error, followed by a new line,
+ * and exit with a status of 98
+ * You are allowed to use more than 5 functions in your file
+ *
+ * @argc: argument count
+ * @argv: argument vectors
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int l1, l2, ln, ti, i;
+	char *a;
+	char *t;
+	char e[] = "Error\n";
+
+	if (argc != 3 || onlyNumbers(argv))
+	{
+		for (ti = 0; e[ti]; ti++)
+			_putchar(e[ti]);
+		exit(98);
+	}
+	for (l1 = 0; argv[1][l1]; l1++)
+		;
+	for (l2 = 0; argv[2][l2]; l2++)
+		;
+	ln = l1 + l2 + 1;
+	a = malloc(ln * sizeof(char));
+	if (a == NULL)
+	{
+		for (ti = 0; e[ti]; ti++)
+			_putchar(e[ti]);
+		exit(98);
+	}
+	init(a, ln - 1);
+	for (ti = l2 - 1, i = 0; ti >= 0; ti--, i++)
+	{
+		t = multiply(argv[2][ti], argv[1], l1 - 1, a, (ln - 2) - i);
+		if (t == NULL)
+		{
+			for (ti = 0; e[ti]; ti++)
+				_putchar(e[ti]);
+			free(a);
+			exit(98);
+		}
+	}
+	_print(a, ln - 1);
+	return (0);
+}
+
+
+
+/**
+ * multiply - multiplies two numbers and displays it
+ * @n: char to multiply
+ * @num: string to multiply
+ * @num_index: last non NULL index of num
+ * @dest: destination of multiplication
+ * @dest_index: highest index to start addition
+ * Return: pointer to dest, or NULL on failure
+ */
+char *multiply(char n, char *num, int num_index, char *dest, int dest_index)
+{
+	int j, k, mul, mulrem, add, addrem;
+
+	mulrem = addrem = 0;
+	for (j = num_index, k = dest_index; j >= 0; j--, k--)
+	{
+		mul = (n - '0') * (num[j] - '0') + mulrem;
+		mulrem = mul / 10;
+		add = (dest[k] - '0') + (mul % 10) + addrem;
+		addrem = add / 10;
+		dest[k] = add % 10 + '0';
+	}
+	for (addrem += mulrem; k >= 0 && addrem; k--)
+	{
+		add = (dest[k] - '0') + addrem;
+		addrem = add / 10;
+		dest[k] = add % 10 + '0';
+	}
+	if (addrem)
+	{
+		return (NULL);
+	}
+	return (dest);
+}
